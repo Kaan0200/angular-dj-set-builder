2cfef2cd070b22fb57bf773e3a0baff4522f9fa4
Alan Kaan Atesoglu <kaan@jagenterprisesllc.com>
2025-07-10 13:35:30 -0400

    added basic home page

---
+++ src/app/pages/HomePage/home.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+
+@Component({
+    selector: 'home-page',
+    templateUrl: './home.html',
+    styleUrl: './home.scss'
+  })
+  export class HomePage {
+    
+  }
+  
