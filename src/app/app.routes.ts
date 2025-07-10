76ce44c7c2d867faa24b91f3fff3a27f390fce03
Alan Kaan Atesoglu <kaan@jagenterprisesllc.com>
2025-07-10 14:15:54 -0400

    added some simple routes, starting to flesh out main structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/HomePage/home';
+import { AboutPage } from './pages/CreditPage/about';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomePage
+    },
+    {
+        path: 'credit',
+        component: AboutPage
     }
 ];
---
+++ src/app/pages/CreditPage/about.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+
+@Component({
+    selector: 'about-page',
+    templateUrl: './about.html',
+    styleUrl: './about.scss'
+  })
+  export class AboutPage {
+    
+  }
+  
