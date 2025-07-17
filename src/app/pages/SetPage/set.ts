9a2150e30b10372ca238f5a8a4aced938659fdd9
Alan Kaan Atesoglu <kaan@jagenterprisesllc.com>
2025-07-17 16:27:01 -0400

    added basic inputs for each song on a tracklist

--- src/app/pages/SetPage/set.ts
+++ src/app/pages/SetPage/set.ts
@@ -1,10 +1,17 @@
 import { Component, inject, input, signal } from '@angular/core';
 import { TrackSet } from '../../models/TrackSet';
+import { MusicTrack } from '../../models/MusicTrack';
 import { ActivatedRoute } from '@angular/router';
 import { LocalStorageService } from '../../services/local-storage.service';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatFabButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 
 @Component({
+    imports: [MatIconModule, MatFabButton, MatFormField, MatInput, MatLabel, MatSlideToggle],
     selector: 'set-page',
     templateUrl: './set.html',
     styleUrl: './set.scss'
@@ -17,14 +24,18 @@ import { LocalStorageService } from '../../services/local-storage.service';
 
     currentTrackSetId = signal('');
 
-    currentTrackSet: TrackSet | null;
+    currentTrackSet: TrackSet;
 
     constructor() {
       console.log(this.activeRoute);
       this.activeRoute.params.subscribe((params) => {
         this.currentTrackSetId.set(params['setId']);
       })
-      this.currentTrackSet = this.localStorageService.getSet(this.currentTrackSetId());
+      this.currentTrackSet = this.localStorageService.getSet(this.currentTrackSetId()) ?? new TrackSet('New Set');
+    }
+
+    AddSong() {
+      this.currentTrackSet.Tracks.push({} as MusicTrack);
     }
   }
   
