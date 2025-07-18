395143bc8d3c8f3c0ec7bd23d6ff0fe41c47b089
Alan Kaan Atesoglu <kaan@jagenterprisesllc.com>
2025-07-18 17:18:45 -0400

    added styles to tame and make fields smaller

--- src/app/pages/SetPage/set.ts
+++ src/app/pages/SetPage/set.ts
@@ -3,15 +3,16 @@ import { TrackSet } from '../../models/TrackSet';
 import { MusicTrack } from '../../models/MusicTrack';
 import { ActivatedRoute } from '@angular/router';
 import { LocalStorageService } from '../../services/local-storage.service';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatFabButton } from '@angular/material/button';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
-    imports: [MatIconModule, MatFabButton, MatFormField, MatInput, MatLabel, MatSlideToggle],
+    imports: [FormsModule, MatIconModule, MatFabButton, MatFormField, MatInput, MatSlideToggle],
     selector: 'set-page',
     templateUrl: './set.html',
     styleUrl: './set.scss'
@@ -35,7 +36,14 @@ import { MatSlideToggle } from '@angular/material/slide-toggle';
     }
 
     AddSong() {
-      this.currentTrackSet.Tracks.push({} as MusicTrack);
+      this.currentTrackSet.Tracks.push({
+        Artist: '',
+        Title: '',
+        Label: '',
+        BMP: '',
+        MusicKey: '',
+        Aquired: false
+      } as MusicTrack);
     }
   }
   
